b02b7485ad56b31a42e54cebc36068255a3d0f69
Mauricio Eduardo Zegarra Puma <mzegarrapu@unsa.edu.pe>
2023-07-17 12:01:44 -0500

    Some models added

---
+++ MyDjangoProject/MyApplication/models/User.py
@@ -0,0 +1,23 @@
+from django.db import models
+from . import User_Type
+import uuid
+
+class User(models.Models):
+    user_id = models.UUIDField()
+    user_type_id = models.ForeignKey(User_Type, related_name="user_type_id", to_field = 'user_type_id')
+    user_dni = models.IntegerField()
+    user_dob = models.DateField()
+    user_email = models.EmailField()
+    user_last_name = models.CharField()
+    user_name = models.CharField()
+    user_password = models.CharField()
+
+    class Meta: 
+        ordering = ['user_name', 'user_id', 'user_type_id', 'user_dni', 'user_email']
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        return super(User, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return "%s %s %s %s %s %s %s %s" % (self.user_id, self.user_type_id, self.user_dni, self.user_dob, self.user_email, self.user_last_name, self.user_name, self.user_passwqrd)
---
+++ MyDjangoProject/MyApplication/models/User_Type.py
@@ -0,0 +1,17 @@
+from django.contrib import models
+import UUID
+
+class User_Tyoe(models.Models):
+    user_type_id = models.UUIDField()
+    user_type_name = models.CharField()
+    user_type_status = models.BooleanField()
+
+    class Meta: 
+        ordering = ['user_type_id', 'user_type_name', 'user_type_status']
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        return super(User, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return "%s %s %s" % (self.user_type_id, self.user_type_name, self.type_status)
