e2c8b7ecae4c54acb545855ddcebeb98cd8e6a67
dohch <dhuamanich@unsa.edu.pe>
2023-07-17 12:27:03 -0500

    models assignment group enrroll

--- MyDjangoProject/MyApplication/models/Enroll.py
+++ MyDjangoProject/MyApplication/models/Enroll.py
@@ -2,10 +2,16 @@ from django.db import models
 import uuid
 
 class Enroll(models.Model):
-    enroll_id=models.UUIDField()
-    group_id=models.ForeignKey(group_id)
-    organization_id=models.ForeignKey(organization_id)
-    student_id=models.ForeignKey(student_id)
-    enroll_cretaed=models.DateTimeField()
-    enroll_modified=models.DateTimeField()
-    enroll_status=models.BooleanField()
+    enroll_id = models.UUIDField()
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
+    organization_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    enroll_created = models.DateTimeField()
+    enroll_modified = models.DateTimeField()
+    enroll_status = models.BooleanField()
+
+    class Meta:
+        ordering = ['enroll_id', 'enroll_created', 'enroll_modified']
+
+    def __str__(self):
+        return "%s %s %s %s" % (self.enroll_id, self.group_id, self.organization_id, self.student_id)
--- MyDjangoProject/MyApplication/models/Group.py
+++ MyDjangoProject/MyApplication/models/Group.py
@@ -2,20 +2,21 @@ from django.db import models
 import uuid
 
 class Group(models.Model):
-    group_id=models.UUIDField()
-    assignment_id=models.ForeignKey(assignment_id)
-    organization_id=models.ForeignKey(organization_id)
-    teacher_id=models.ForeignKey(teacher_id)
-    group_cretaed=models.DateTimeField()
-    group_modified=models.DateTimeField()
-    group_name=models.CharField()
-    group_status=models.BooleanField()
+    group_id = models.UUIDField()
+    assignment_id = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    organization_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    teacher_id = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    group_created = models.DateTimeField()
+    group_modified = models.DateTimeField()
+    group_name = models.CharField(max_length=255)
+    group_status = models.BooleanField()
+
+    class Meta:
+        ordering = ['group_id', 'group_created', 'group_modified']
     
     def save(self, *args, **kwargs):
-        self.names = self.names.upper()
-        self.father_surname = self.father_surname.upper()
-        self.mother_surname = self.mother_surname.upper()
-        return super(Student, self).save(*args, **kwargs)
+        self.group_name = self.group_name.upper()
+        return super(Group, self).save(*args, **kwargs)
     
     def __str__(self):
-        return " %s %s %s %s" % (self.cui, self.names, self.father_surname,self.mother_surname)
+        return "%s %s %s %s" % (self.group_id, self.assignment_id, self.organization_id, self.teacher_id)
